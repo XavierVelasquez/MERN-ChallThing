855fd719168890ea9ead281530784bb94c2ab9d9
XavierVelasquez <xaviervelasquez@gmail.com>
2023-03-08 19:31:01 -0800

    Mostly done but need to move stuff

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import SearchBooks from "./pages/SearchBooks"
+import SavedBooks from "./pages/SavedBooks"
+import Navbar from "./components/Navbar"
+
+// Import the Apollo client.
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client"
+
+// Create the Apollo client.
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
-  );
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
